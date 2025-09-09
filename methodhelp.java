b78eb56b7a41d0532fda6399fbf87135ff3781fd
Efaz Khan <efazkhan2009@gmail.com>
2025-09-08 20:17:35 -0400

    test2

---
+++ methodhelp.java
@@ -0,0 +1,26 @@
+  import java.io.**;
+  import java.util.**;
+
+  public class Hello {
+
+      public static void printHello(String name){
+          String result;
+          result = returnHello(name);
+          System.out.println(result);
+      }
+
+      public static String returnHello(String name){
+          String result;
+          return "Hello " + name + "!";
+      }
+
+      public static void main(String[] args){
+          String s;
+          printHello("Thomas");
+          s = returnHello("Jane");
+          System.out.println(s);
+      }
+
+  }
+  
+  
